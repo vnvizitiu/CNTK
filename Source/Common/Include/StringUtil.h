f1aac4ace42acea99bbd3f970fea4578f0ea70b9
Eldar Akchurin <eldak@microsoft.com>
2016-02-19 17:28:33 +0100

    Adding ImageReader back

--- Source/Common/Include/StringUtil.h
+++ Source/Common/Include/StringUtil.h
@@ -22,4 +22,22 @@ inline bool AreEqualIgnoreCase(
     });
 }
 
+template<class TElement>
+inline bool AreEqualIgnoreCase(
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement>>& s1,
+    const TElement* s2pointer)
+{
+    std::basic_string<TElement, char_traits<TElement>, allocator<TElement>> s2(s2pointer);
+    return AreEqualIgnoreCase(s1, s2);
+}
+
+template<class TElement>
+inline bool AreEqualIgnoreCase(
+    const TElement* s1pointer,
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement>>& s2)
+{
+    std::basic_string<TElement, char_traits<TElement>, allocator<TElement>> s1(s1pointer);
+    return AreEqualIgnoreCase(s1, s2);
+}
+
 }}}
